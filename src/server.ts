58ae4b7a4b9b36dff78fd3b5b32b900a26e7ebcd
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-09-02 09:53:29 -0300

    feat: global error handling with express middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { json } from 'express';
+import 'express-async-errors';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import router from './routers';
+import errorHandler from './middlewares/errorHandlingMiddleware';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -8,6 +11,9 @@ const app = express();
 app.use(json());
 app.use(cors());
 
+app.use(router);
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
-})
+});
